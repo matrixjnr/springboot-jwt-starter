6ef7dea31f60d3d186b948bd8a266c908bbb12c1
matrixjnr <jonnyeinstein@gmail.com>
2022-01-30 02:59:58 +0300

    models

---
+++ src/main/java/eu/vfvirtual/starter/StarterApplication.java
@@ -0,0 +1,13 @@
+package eu.vfvirtual.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterApplication.class, args);
+	}
+
+}
---
+++ src/main/java/eu/vfvirtual/starter/model/UserRoleName.java
@@ -0,0 +1,6 @@
+package eu.vfvirtual.starter.model;
+
+public enum UserRoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
